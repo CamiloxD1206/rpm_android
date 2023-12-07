f67bec087caa0bb752f533640f577d632286b104
Camilo Tacue Salazar <cetacue@misena.edu.co>
2023-12-06 19:31:10 -0500

    borre demas cosas dejando solo las 2 vistas y la preparacion de menu

--- app/src/main/java/com/example/pruebas/HomeActivity.kt
+++ app/src/main/java/com/example/pruebas/HomeActivity.kt
@@ -1,12 +1,9 @@
 package com.example.pruebas
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import com.example.pruebas.databinding.ActivityRegisterBinding
 
-class RegisterActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
