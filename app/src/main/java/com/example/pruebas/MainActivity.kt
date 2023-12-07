1cd74d3be5285d6c5365f4c0fb5344de9c8bff32
Camilo Tacue Salazar <cetacue@misena.edu.co>
2023-12-06 19:44:15 -0500

    configuracion viewbinding

--- app/src/main/java/com/example/pruebas/MainActivity.kt
+++ app/src/main/java/com/example/pruebas/MainActivity.kt
@@ -3,23 +3,16 @@ package com.example.pruebas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pruebas.databinding.ActivityHomeBinding
 import com.example.pruebas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
-        binding.btnRegistro.setOnClickListener{
-            var intent=Intent(this,RegisterActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
-
-
-
 }
--- app/src/main/java/com/example/pruebas/RegisterActivity.kt
+++ app/src/main/java/com/example/pruebas/RegisterActivity.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import com.example.pruebas.databinding.ActivityHomeBinding
 import com.example.pruebas.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
