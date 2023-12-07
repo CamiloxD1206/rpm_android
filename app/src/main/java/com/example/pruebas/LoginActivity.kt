755eacca6d41f41204d612a46d2fe7160a2133db
Camilo Tacue Salazar <cetacue@misena.edu.co>
2023-12-06 22:17:25 -0500

    camilo

--- app/src/main/java/com/example/pruebas/LoginActivity.kt
+++ app/src/main/java/com/example/pruebas/LoginActivity.kt
@@ -3,21 +3,18 @@ package com.example.pruebas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pruebas.databinding.ActivityLoginBinding
 import com.example.pruebas.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-        binding.btnregister.setOnClickListener{
-            var intent=Intent(this,LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+
     }
 
 
