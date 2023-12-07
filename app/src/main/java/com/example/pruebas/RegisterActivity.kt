3c37bc295d94c8908084e5b20717b40ab32ecd0f
Camilo Tacue Salazar <cetacue@misena.edu.co>
2023-12-07 08:53:29 -0500

    main ajustado

--- app/src/main/java/com/example/pruebas/RegisterActivity.kt
+++ app/src/main/java/com/example/pruebas/RegisterActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import com.example.pruebas.databinding.ActivityMainBinding
 import com.example.pruebas.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
